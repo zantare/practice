41948555b77c28937f696357c27f9651b0d8b981
tonozaki <tonozakitkys@gmail.com>
2009-12-01 16:14:58 +0900

    add repeat

---
+++ tonozaki/repeat.rb
@@ -0,0 +1,26 @@
+# 数分繰り返す
+3.times {
+	puts "hello"
+}
+
+# upto
+x = 1
+x.upto(3) {
+	puts "bye"
+}
+
+# 条件を満たしている間
+x = 1
+while x < 3
+	puts "こんにちわ！"
+	x += 1
+end
+
+# 条件になるまで
+x = 1
+until x > 2
+	puts "こんばんわ！"
+	x += 1
+end
+
+
