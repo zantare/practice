bb230e30c7d5af8949abda83bd58642c3bd64847
otomo <the50mile@gmail.com>
2009-12-01 18:00:06 +0900

    add button.rb method

--- otomo/button.rb
+++ otomo/button.rb
@@ -1,8 +1,21 @@
 class Button
+	attr_reader :created_at
+
+	def initialize
+		@created_at = Time.now
+	end
+
 	def push
+		action
+	end
+
+	private # 内部的なmethod
+	def action
 		puts "push!"
 	end
+
 end
 
 b = Button.new
 b.push
+puts b.created_at.strftime("%Y-%m-%d")
