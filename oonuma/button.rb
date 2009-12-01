9e36d37dcfa9031714d1336bb92ab57ceaafd183
masahito <oonumaster@gmail.com>
2009-12-01 17:39:22 +0900

    add coloums

--- oonuma/button.rb
+++ oonuma/button.rb
@@ -1,8 +1,20 @@
 class Button
- def push
+	attr_reader :created_at
+	
+	def initialize
+		@created_at = Time.now
+	end
+
+	def push
+		action
+	end
+
+ private
+ def action
   puts "push!"
  end
 end
 
 b = Button.new
 b.push
+puts b.created_at.strftime("%Y-%m-%d")
