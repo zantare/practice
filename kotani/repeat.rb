99d93ef60b03ddbaa0331e8af588e3d6a791ecf2
Masaki Kotani <papapo06@mail.goo.ne.jp>
2009-12-01 16:15:02 +0900

    new repeat.rb

---
+++ kotani/repeat.rb
@@ -0,0 +1,21 @@
+#回数を指定
+10.times{
+	puts "hello"
+}
+#xになるまで
+x = 1
+x.upto(5){
+	puts "bye"
+}
+#条件を満たしている間
+x = 1
+while x < 5
+	puts "こんにちわ！！"
+	x += 1
+end
+#条件を満たしている間
+x = 1
+until x > 5
+	puts "こんばんわ＾＾"
+	x += 1
+end
