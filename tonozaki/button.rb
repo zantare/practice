c183ed7f031cfa98c0a8dc185e19cf1860064db2
tonozaki <tonozakitkys@gmail.com>
2009-12-01 17:37:49 +0900

    add class button 改造

--- tonozaki/button.rb
+++ tonozaki/button.rb
@@ -1,8 +1,26 @@
 class Button
+	# attr_reader：外部参照用変数
+	attr_reader :created_at
+
+	# initialize：初期化
+	def initialize
+		# @：オブジェクト内で参照する
+		@created_at = Time.now
+	end
+
 	def push
+		action
+	end
+
+	# 内部処理
+	private
+	def action
 		puts "push!"
 	end
 end
 
 b = Button.new
 b.push
+
+# attr_readerでcreated_atが参照できる
+puts b.created_at.strftime("%Y-%m-%d")
